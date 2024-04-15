316323b7bd0046fe4df8a2e2b8fc52789d9fce85
Abhay Pratap singh <63098388+abhay23singh@users.noreply.github.com>
2024-04-15 10:07:07 +0000

    comment

--- array/new.js
+++ array/new.js
@@ -13,7 +13,7 @@ console.log(newArray);
 console.log(newArray.pop());
 console.log(newArray);
 
-// shift element+++++++++++++++++++
+//shift element+++++++++++++++++++
 
 newArray.shift();
 console.log(newArray);
@@ -22,4 +22,15 @@ console.log(newArray);
 
 newArray.unshift(23);
 console.log(newArray);
+
+//slice and splice+++++++++++++++++
+
+const arr= [3,4,5,6,7,1]
+const slicearr= arr.slice(1,3);
+console.log(slicearr);
+console.log(arr);
+//splice array
+const splicearr=arr.splice(1,3);
+console.log(splicearr);
+console.log(arr);
 
