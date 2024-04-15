d378f7d9626c08bd20ef0d2db3abe5d481cb05ca
Abhay Pratap singh <63098388+abhay23singh@users.noreply.github.com>
2024-04-15 10:51:01 +0000

    comments about push concat

--- array/new.js
+++ array/new.js
@@ -24,13 +24,24 @@ newArray.unshift(23);
 console.log(newArray);
 
 //slice and splice+++++++++++++++++
+//it doesn't manipulate the original array
 
 const arr= [3,4,5,6,7,1]
 const slicearr= arr.slice(1,3);
+console.log(arr);
 console.log(slicearr);
-console.log(arr);
-//splice array
-const splicearr=arr.splice(1,3);
-console.log(splicearr);
-console.log(arr);
 
+
+//splice array
+//Splice will manipulate the original array while getting the splice element 
+
+const splicearr=arr.splice(1,3);
+console.log(arr);
+console.log(splicearr);
+
+
+//we can use "PUSH" element to add to array but the main arraqy will treat that array,
+//as a new array element. 
+//we s hould use "CONCAT" instead of "PUSH"
+
+const anotherarr= [1,2,3,[4,5,2,9,[33,566,43]]]
