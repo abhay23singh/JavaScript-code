11b52c8dc83290867655c8ded6dec9b054352281
Abhay Pratap singh <63098388+abhay23singh@users.noreply.github.com>
2024-04-16 09:19:24 +0000

    functions

--- functions/1function.js
+++ functions/1function.js
@@ -1,3 +1,23 @@
-function new1(){
-    console.log("Abhay Pratap Singh");
+
+function myname(){
+console.log("A");
+console.log("B");
+console.log("H");
+console.log("A");
+console.log("Y");
 }
+myname() //myname is REFERENCE , () is Execution 
+
+function add2no(number1,number2){  //(number1,number2) this is parameter
+    console.log(number1+number2);
+}
+add2no(4,5)//4,5 is arguments
+
+function addtwono(number1,number2){
+
+    let result=number1 + number2
+    return result
+}
+const result=addtwono(19,20)
+console.log("Result:",result);
+
---
+++ functions/2function.js
@@ -0,0 +1,37 @@
+// Shoping Cart 
+
+// (...) Rest operator 
+//by using this we can use multiple value in form of array 
+
+function calculateCarPrice(...num1){
+    return num1
+
+}
+console.log(calculateCarPrice(200,300,500))
+
+//object handeling in function
+
+const user ={
+    username:"abhay",
+    price:199
+}
+
+function handelObject(anyobject){
+    console.log(`User is ${anyobject.username} and price is ${anyobject.price}.`);
+}
+
+// handelObject(user)
+handelObject({
+    username:"ABHAY",
+    price:199
+})
+
+//Array handeling in js function
+
+const newarray=[200,300,400,500]
+
+function secondvalue(getArray){
+    return getArray
+}
+// console.log(secondvalue(newarray));
+console.log(secondvalue([200,300,400,500]))
