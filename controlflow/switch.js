ed1515074edc4fc7c6299fd1ff2c2961f7945857
Abhay Pratap singh <63098388+abhay23singh@users.noreply.github.com>
2024-04-20 22:35:30 +0000

    switch nullish and truthy falsy

---
+++ controlflow/switch.js
@@ -0,0 +1,37 @@
+const month="october"
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "october":
+        console.log("October");
+        break;
+
+    default:
+        console.log("print default");
+        break;
+}
+
+
+//+++++++++++++++++++ Truthy & Falsy Values +++++++++++++++++++++
+
+
+//false value
+// false, 0, -0, BigInt, 0n, "", null, undefined, NaN
+
+//truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+//Nullish coaleshing Operator (??): null undefined 
+
+let val1;
+// val1 = 5 ?? 10;
+val1 =   null ?? 10;
+// val1 = undefined ?? 5;
+console.log(val1);
