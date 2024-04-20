2efc99e857af4f90b1a010d5c88835717b562269
Abhay Pratap singh <63098388+abhay23singh@users.noreply.github.com>
2024-04-20 22:09:48 +0000

    one

---
+++ controlflow/one.js
@@ -0,0 +1,27 @@
+//+++++++++++++++IF++++++++++++++ 
+
+
+// const IsUserloggedIn =true
+
+// if(2 === "2")//nothing is printed
+// {
+//     console.log("executed");
+// }
+
+// <,>,<=,>=,==,!=,===(Strict equal {check data type is also equal})
+
+
+//+++++++++++scope understanding+++++++++++++++
+
+
+// const score =200
+
+// if(score  > 100){
+//     const power ="fly"
+//     console.log(`User Power: ${power}`);
+// }
+// console.log(`User Power: ${power}`);   // it wouldn't  run 
+//because power doesn't have a global scope it only work in if scope 
+// if we use var insted of const then it will run eaily , but it's  not good
+
+
