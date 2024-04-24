71990e29bcf3452dea04729a483d57994cf898f1
Abhay Pratap singh <abhay.singhchandel18@gmail.com>
2024-04-25 01:35:28 +0530

    extra foreach

--- itteration/extendforeach.js
+++ itteration/extendforeach.js
@@ -13,19 +13,41 @@ const nums= myNums.filter((num)=> num > 4) // explicitly return the value
 const Nums= myNums.filter((num)=> {
     return num > 4
 }) // return should be used to return the value
+// console.log(nums);
 
-
-console.log(nums);
 
 // now we can also do that by using for each 
+
 
 const myNums2=[1,2,3,4,5,6,7,8,9,10]
 
 myNums2.forEach((nums)=>{
     if(nums > 4){
-        console.log(nums);
+        // console.log(nums);
     }
 })
 
 
+// Now I am doing this whole thing by using  map method
 
+
+const myNums3=[1,2,3,4,5,6,7,8,9,10]
+const nums2= myNums3.map((num)=>{
+    return num + 10
+})
+// console.log(nums2);
+
+// let's see how to implement chaining 
+
+const myNums4=[1,2,3,4,5,6,7,8,9,10]
+const nums3= myNums4
+.map((num)=>{
+    return num  * 10
+})
+.map((num)=>{
+    return num + 1
+})
+.filter((num)=>{
+    return num > 40
+})
+console.log(nums3);
